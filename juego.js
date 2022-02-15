73a87c5cd0553b21b8628398ea2ff8cb6bdbfffb
Lucas Blandon <lucasblandon@gmail.com>
2022-02-15 18:29:11 -0500

    funcion de js fin al

--- juego.js
+++ juego.js
@@ -12,9 +12,36 @@ function cuadroRandom(){
     cuadro.forEach(cuadro => {
         cuadro.classList.remove('fantasma')
     })
-
     let cuadroRandom = cuadro[Math.floor(Math.random() *9)]
     cuadroRandom.classList.add('fantasma')
-
     lugarGolpe = cuadroRandom.id
 }
+
+cuadro.forEach(cuadro =>{
+  cuadro.addEventListener('mousedown', ()=>{
+    if (cuadro.id == lugarGolpe){
+      resultado++
+      puntos.textContent = resultado
+      lugarGolpe = null
+    }
+  })
+})
+
+function moverFantasma(){
+  idTiempo = setInterval(cuadroRandom, 500)
+}
+
+moverFantasma()
+
+function contadorRegresivo(){
+  tiempoInicial--
+  tiempo.textContent = tiempoInicial
+
+  if(tiempoInicial==0){
+    clearInterval(conteoTiempoId)
+    clearInterval(idTiempo)
+    alert('GAME OVER!! Su puntaje es: '+ resultado )
+  }
+}
+
+let conteoTiempoId = setInterval(conteo, 1000)
